483ca6b4ef78fcf91fc6255ec2dbdddac3995f2d
Xancia <cminhma@gmail.com>
2024-12-10 14:58:48 -0500

    Went over basics of csharp

--- week1/Basic CSharp/Program.cs
+++ week1/Basic CSharp/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+String input = Console.ReadLine() ?? "";
+Console.WriteLine(input.Trim());
+
+// Primatives
+/*
+Whole Numbers
+int 32 bit
+long 64 bit
+short 16 bit
+byte 8 bit
+
+Decimal Numbers
+double 64bit
+float 32bit 9.654F
+decimal biggest bit
+
+Boolean
+bool
+
+Single character
+char 'c'
+
+String - NOT a primative - it is an object
+string myString = "Hello"
+*/
+
+// Math
+/*
++ +=
+- -=
+/ /=
+* *=
+% modular
+
+Comparison
+==
+!=
+.Equals
+>
+<
+>=
+<=
+
+Logical Operators
+&& and
+|| or
+! not operator
+
+
+Bitwise operators - check for myself
+*/

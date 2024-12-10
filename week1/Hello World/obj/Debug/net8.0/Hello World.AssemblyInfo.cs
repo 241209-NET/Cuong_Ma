4a07ba6ab5e745a25a69cbd95b018d0f8d4949fe
Xancia <cminhma@gmail.com>
2024-12-10 16:26:52 -0500

    git ignore

--- week1/Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
+++ week1/Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9eb3d237b2e807e65ce718c6741295fc8abc44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8962040f52df56c9d4519a664161ebcacfb1c6f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

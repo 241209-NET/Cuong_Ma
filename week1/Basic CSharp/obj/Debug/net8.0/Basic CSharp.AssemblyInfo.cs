206ddeab119dee9d98741cd8b41cd34b44bc4572
Xancia <cminhma@gmail.com>
2024-12-10 14:17:50 -0500

    vscode files

--- week1/Basic CSharp/obj/Debug/net8.0/Basic CSharp.AssemblyInfo.cs
+++ week1/Basic CSharp/obj/Debug/net8.0/Basic CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9eb3d237b2e807e65ce718c6741295fc8abc44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9776daeb72ae1b5eefb7f4818a114dabb8b6ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

8962040f52df56c9d4519a664161ebcacfb1c6f3
Xancia <cminhma@gmail.com>
2024-12-10 16:18:55 -0500

    Fizzbuzz excercise

--- week1/Basic CSharp/Program.cs
+++ week1/Basic CSharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-String input = Console.ReadLine() ?? "";
-Console.WriteLine(input.Trim());
+// Console.WriteLine("Hello, World!");
+// String input = Console.ReadLine() ?? "";
+// Console.WriteLine(input.Trim());
 
 // Primatives
 /*
@@ -51,3 +51,35 @@ Logical Operators
 
 Bitwise operators - check for myself
 */
+
+//Fizzbuzz exercise.
+
+System.Console.WriteLine("Enter a Number for Fizzbuzz");
+string? input = Console.ReadLine();
+if (int.TryParse(input, out int n))
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            System.Console.WriteLine("FizzBuzz");
+        }
+        else if (i % 5 == 0)
+        {
+            System.Console.WriteLine("Buzz");
+        }
+        else if (i % 3 == 0)
+        {
+            System.Console.WriteLine("Fizz");
+        }
+        else
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a number.");
+}
+;

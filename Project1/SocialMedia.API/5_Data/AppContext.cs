b9c670a7a5554af5627975cc0859ce2fcf158362
Xancia <cminhma@gmail.com>
2024-12-23 10:40:53 -0500

    added appcontext

---
+++ Project1/SocialMedia.API/5_Data/AppContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.API.Model;
+
+namespace SocialMedia.API.Data;
+
+public partial class AppContext : DbContext
+{
+    public AppContext() { }
+
+    public AppContext(DbContextOptions<AppContext> options)
+        : base(options) { }
+
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<Tweet> Tweets { get; set; }
+}

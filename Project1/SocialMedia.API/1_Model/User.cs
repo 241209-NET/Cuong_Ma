6b4c138d78c9d013a814081433fecdf39a4cb1da
Xancia <cminhma@gmail.com>
2024-12-27 03:45:29 -0500

    added json ignore to stop cycles, everything works now

--- Project1/SocialMedia.API/1_Model/User.cs
+++ Project1/SocialMedia.API/1_Model/User.cs
@@ -1,10 +1,13 @@
 namespace SocialMedia.API.Model;
 
+using System.Text.Json.Serialization;
+
 public class User
 {
     public int Id { get; set; }
     public required string Username { get; set; }
     public required string Password { get; set; }
 
+    [JsonIgnore]
     public ICollection<Tweet> Tweets { get; set; } = [];
 }
